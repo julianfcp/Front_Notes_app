b0711b9bf15edb054f97ab056c34d3bb22c4f716
Julian <julian.fcp@gmail.com>
2020-03-09 23:16:38 -0500

    highlight the menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,9 +2,35 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 
 export default class Navigation extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            aNotes: 'active',
+            aCreateNote: '',
+            aCreateUser: ''
+        }
+    }
+
+    onClickLink = (e) => {
+        switch (e.target.id) {
+            case "aNotes": 
+                this.setState({aNotes: 'active', aCreateNote: '', aCreateUser: ''})
+                break;
+            case "aCreateNote": 
+                this.setState({aNotes: '', aCreateNote: 'active', aCreateUser: ''})
+                break;
+            case "aCreateUser": 
+                this.setState({aNotes: '', aCreateNote: '', aCreateUser: 'active'})
+                break;
+            default:
+                break;
+        }
+    }
+
+
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
                 <div className="container">
                     <Link className="navbar-brand" to="/">
                         Notes App
@@ -14,14 +40,14 @@ export default class Navigation extends Component {
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav ml-auto">
-                            <li className="nav-item active">
-                                <Link className="nav-link" to="/">Notes</Link>
+                            <li className={"nav-item "+this.state.aNotes}>
+                                <Link id="aNotes" className="nav-link" to="/" onClick={this.onClickLink}>Notes</Link>
                             </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/create">Create Note</Link>
+                            <li className={"nav-item "+this.state.aCreateNote}>
+                                <Link id="aCreateNote" className="nav-link" to="/create" onClick={this.onClickLink}>Create Note</Link>
                             </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/user">Create User</Link>
+                            <li className={"nav-item "+this.state.aCreateUser}>
+                                <Link id="aCreateUser" className="nav-link" to="/user" onClick={this.onClickLink}>Create User</Link>
                             </li>
                         </ul>
                     </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'jquery';
+import 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
